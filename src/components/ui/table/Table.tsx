07f1a9d2b4b6bdd91c8be9bf26500c0e21d38937
Grigoriy Kolenko <kolenko@me.com>
2020-03-27 22:21:48 +0100

    Add base responsivity

--- src/components/ui/table/Table.tsx
+++ src/components/ui/table/Table.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Callout, Colors, Icon, IHTMLTableProps } from '@blueprintjs/core';
 import { Column, useSortBy, useTable } from 'react-table';
 
-import { BaseTable, TH, THBackground } from './styled';
+import { BaseTable, TableWrapper, TH, THBackground } from './styled';
 
 export type TableColumns<T extends object> = Column<T>[];
 
@@ -26,57 +26,59 @@ const TableMemo: React.FC<TableProps<any>> = (props) => {
   }, 0);
 
   return (
-    <BaseTable {...tableProps} {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup, index) => (
-          <tr key={index} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column, i) => (
-              // Add the sorting props to control sorting. For this example
-              // we can add them into the header props
-              <TH
-                // active={(intersection?.intersectionRatio || 1) < 1}
-                key={i}
-                {...column.getHeaderProps((column as any).getSortByToggleProps())}
-              >
-                {column.render('Header')}
-                <THBackground />
-                {/* Add a sort direction indicator */}
-                <span>
-                  {(column as any).isSorted ? (
-                    <Icon color={Colors.GRAY4} icon={(column as any).isSortedDesc ? 'caret-up' : 'caret-down'} />
-                  ) : (
-                    ''
-                  )}
-                </span>
-              </TH>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.length === 0 && (
-          <tr>
-            <td colSpan={totalColumns}>
-              <Callout intent="none">No content</Callout>
-            </td>
-          </tr>
-        )}
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr key={i} {...row.getRowProps()}>
-              {row.cells.map((cell, index) => {
-                return (
-                  <td key={index} {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
+    <TableWrapper>
+      <BaseTable {...tableProps} {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup, index) => (
+            <tr key={index} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, i) => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <TH
+                  // active={(intersection?.intersectionRatio || 1) < 1}
+                  key={i}
+                  {...column.getHeaderProps((column as any).getSortByToggleProps())}
+                >
+                  {column.render('Header')}
+                  <THBackground />
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {(column as any).isSorted ? (
+                      <Icon color={Colors.GRAY4} icon={(column as any).isSortedDesc ? 'caret-up' : 'caret-down'} />
+                    ) : (
+                      ''
+                    )}
+                  </span>
+                </TH>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </BaseTable>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.length === 0 && (
+            <tr>
+              <td colSpan={totalColumns}>
+                <Callout intent="none">No content</Callout>
+              </td>
+            </tr>
+          )}
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr key={i} {...row.getRowProps()}>
+                {row.cells.map((cell, index) => {
+                  return (
+                    <td key={index} {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </BaseTable>
+    </TableWrapper>
   );
 };
 
--- src/pages/home/styled.ts
+++ src/pages/home/styled.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div({
   display: 'flex',
-  justifyContent: 'space-between'
+  justifyContent: 'space-between',
+  flexWrap: 'wrap'
 });
