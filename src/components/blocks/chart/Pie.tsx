e0e13348f3b4754dedd139c793f2797595a568dc
Alex Alexeev <work2alex@gmail.com>
2020-03-27 19:33:36 +0100

    Fix sort mutation

--- src/components/blocks/chart/Pie.tsx
+++ src/components/blocks/chart/Pie.tsx
@@ -11,20 +11,19 @@ export type PieProps = {};
 const PieMemo: React.FC<PieProps> = (props) => {
   const { state } = useCountries();
   const loading = state.status === StoreStatus.Fetching;
-  const data = state?.content?.sort((a, b) => (a.todayCases >= b.todayCases ? -1 : 1));
+  const data = state.content || [];
+  const sortedData = [...data].sort((a, b) => (a.todayCases >= b.todayCases ? -1 : 1));
 
   return (
-    <>
-      <Segment loading={loading} style={{ padding: 0 }}>
-        {!state.error && (
-          <LineChart width={800} height={400} data={data ? [...data].splice(0, 5) : []}>
-            <Line type="monotone" dataKey="todayCases" stroke="#8884d8" />
-            <XAxis dataKey="country" />
-            <YAxis />
-          </LineChart>
-        )}
-      </Segment>
-    </>
+    <Segment loading={loading} style={{ padding: 0 }}>
+      {!state.error && (
+        <LineChart width={800} height={400} data={sortedData.slice(0, 6)}>
+          <Line type="monotone" dataKey="todayCases" stroke="#8884d8" />
+          <XAxis dataKey="country" />
+          <YAxis />
+        </LineChart>
+      )}
+    </Segment>
   );
 };
 
